bbf20e39191f67ccb343c80c3bf9da604b58a92b
morrss <grishkovea@gmail.com>
2025-01-04 02:08:41 +0300

    fix: почему-то не работает lombok, пришлось вручную писать геттеры, сеттеры и toString

--- src/main/java/com/morrs/firstrestapp/models/Scanner.java
+++ src/main/java/com/morrs/firstrestapp/models/Scanner.java
@@ -2,13 +2,31 @@ package com.morrs.firstrestapp.models;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import lombok.Data;
+
+import java.util.List;
 
 @Entity
 @Table(name = "scanners")
-@Data
 public class Scanner {
     @Id
     private String name;
+
+    @OneToMany(mappedBy = "scanner")
+    private List<ScannersData> data;
+
+    @Override
+    public String toString() {
+        return "Scanner{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
 }
