fe5651009a20e120c49de5de2344310ee87aecce
morrss <grishkovea@gmail.com>
2025-01-04 02:10:40 +0300

    feat: добавил репо для сканера

---
+++ src/main/java/com/morrs/firstrestapp/repos/ScannersRepo.java
@@ -0,0 +1,11 @@
+package com.morrs.firstrestapp.repos;
+
+import com.morrs.firstrestapp.models.Scanner;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ScannersRepo extends JpaRepository<Scanner, UUID> {
+}
