3d7bd6e6773db05c7a946002c1d26ebe95638853
morrss <grishkovea@gmail.com>
2025-01-05 13:24:22 +0300

    feat: добавил валидатор на класс дто сканера

--- src/main/java/com/morrs/firstrestapp/dto/ScannerDTO.java
+++ src/main/java/com/morrs/firstrestapp/dto/ScannerDTO.java
@@ -1,4 +1,8 @@
 package com.morrs.firstrestapp.dto;
 
-public record ScannerDTO(String name) {
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record ScannerDTO(@NotNull(message = "Name must be not empty")
+                         @Size(min = 3, max = 30, message = "Length must me more than 3 and less then 30") String name) {
 }
