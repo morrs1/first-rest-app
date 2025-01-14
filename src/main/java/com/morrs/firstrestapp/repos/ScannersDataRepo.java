9837ab89b03cd5d2fe268bd9656857804ad4f0ec
morrs <grishkovea@gmail.com>
2025-01-15 00:20:49 +0300

    feat: добавил метод для поиска кол-ва дождливых дней

--- src/main/java/com/morrs/firstrestapp/repos/ScannersDataRepo.java
+++ src/main/java/com/morrs/firstrestapp/repos/ScannersDataRepo.java
@@ -2,8 +2,14 @@ package com.morrs.firstrestapp.repos;
 
 import com.morrs.firstrestapp.models.ScannersData;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ScannersDataRepo extends JpaRepository<ScannersData, Integer> {
+    boolean existsByScannerName(String scannerName);
+
+    @Query(value = "SELECT COUNT(*) FROM scanners_data WHERE raining = TRUE", nativeQuery = true)
+    int countRaining();
+
 }
