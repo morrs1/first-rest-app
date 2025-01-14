b63ea6a7e578f5869152bc34a7dd32e181c36c15
morrs <grishkovea@gmail.com>
2025-01-14 23:03:21 +0300

    fix: отрефакторил дто сканнера, чтобы выглядело получше

--- src/main/java/com/morrs/firstrestapp/dto/ScannerDTO.java
+++ src/main/java/com/morrs/firstrestapp/dto/ScannerDTO.java
@@ -3,6 +3,9 @@ package com.morrs.firstrestapp.dto;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
-public record ScannerDTO(@NotNull(message = "Name must be not empty")
-                         @Size(min = 3, max = 30, message = "Length must me more than 3 and less then 30") String name) {
+public record ScannerDTO(
+        @NotNull(message = "Name must be not empty")
+        @Size(min = 3, max = 30, message = "Length must me more than 3 and less then 30")
+        String name
+) {
 }
